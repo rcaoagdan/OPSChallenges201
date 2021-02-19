085be4880f68e2776b29c0307977933ae2f18ad6
rcaoagdan <rcaoagdan@icloud.com>
2021-02-18 19:32:51 -0800

    newchallenge

---
+++ challenge4.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+#Script: Ops Challenge 4
+#Author: Ray Caoagdan
+#Date of Last Revision: 02/18/2021
+#Purpose: Arrays :Create 4 directories | Putting 4 directories in an array | Reference the array variable to create new text files in each directory
+
+
+#making directory using loop
+make_dir (){
+    for i in {1..4}
+    do
+        command mkdir "dir$i"
+    done
+}
+
+make_dir
+
+
+
+#Declaation of Functions
+
+#main
+
+
+#end
