bff04723896ebb3dc76feebccf39a3e1efe31698
rcaoagdan <rcaoagdan@icloud.com>
2021-02-20 10:25:02 -0800

    update

--- challenge5.sh
+++ challenge5.sh
@@ -13,7 +13,7 @@ target=$(ps aux) #ps aux lists all processes
 #asks to kill
 ask_to_kill() {
 	echo -e "Shall we kill a process? y/n?: "
-	read -r  option #looks at the input y/n then proceeds to functions below accordingly
+	read -r  option 
 	while :
 	do
  		if [ "$option" == n ] ; then #if option is n  run commands below
@@ -30,7 +30,7 @@ kill_process() {
 	if [ "$option" == y ]; then #if option is y then run commands below
 		echo -e "$target" 
 		echo -e "Select your target"
-		read -r PID #reads PID chosen then proceeds to kill -9
+		read -r PID 
 		kill -9  "$PID" #kills -9 the process
 
 		ask_to_kill
