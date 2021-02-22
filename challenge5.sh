04eb0eeb28a47f10d8591312f5adceda9ee52113
rcaoagdan <rcaoagdan@icloud.com>
2021-02-21 16:19:51 -0800

    update

--- challenge5.sh
+++ challenge5.sh
@@ -9,11 +9,9 @@
 process=$(ps aux) #ps aux lists all processes
 
 #main
+print_process
+
 kill_reset () {
-	echo -e "$process"
-	echo -e "\nHello for your convience the process will aslo be printed in output.txt"
-	cat /dev/null >output.txt #deletes any content currently in output.txt 
-	echo -e "$process" >>output.txt #prints PID in output.txt 
 	echo -e "\nShall we kill-k or reset-r a process? k/r?" #ask to kill or reset
 	read -r option #reads user input
 	while :
@@ -31,6 +29,13 @@ kill_reset () {
 #end
 
 #below are the fuction called upon to main
+
+print_process() {
+	echo -e "$process"
+	echo -e "\nHello for your convience the process will aslo be printed in output.txt"
+	cat /dev/null >output.txt #deletes any content currently in output.txt 
+	echo -e "$process" >>output.txt #prints PID in output.txt 
+}
 
 #kill the process
 kill_process() {
