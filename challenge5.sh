5052e6291727d814a339a5bbb915a0ed7455b6c9
rcaoagdan <rcaoagdan@icloud.com>
2021-02-21 16:01:28 -0800

    update

--- challenge5.sh
+++ challenge5.sh
@@ -2,7 +2,7 @@
 
 #Script Name: OPS 201 Challenge 5
 #Author: Ray Caoagdan
-#Date of last revision: 02/20/2021
+#Date of last revision: 02/21/2021
 #Description of Purpose: Use Loops- Ask user for PID then kills process with PID
 
 #declare variables
@@ -27,6 +27,11 @@ kill_reset () {
 
 }
 
+#end
+
+#below are the fuction called upon to main
+
+#kill the process
 kill_process() {
 	if [ "$option" == k ]; then
 		echo -e "Select process found in output.txt"
@@ -37,57 +42,19 @@ kill_process() {
 	fi
 }
 
+#reset the process
 reset_process () {
 	if [ "$option" == r ]; then
 		echo -e "Select Process found in output.txt"
 		read -r PID
 		kill -15 "$PID"
+		echo -e "$PID has been reseted"
 
 		kill_reset
 	fi
 }
 
 kill_reset
-
-#end
-
-
-
-
-
-
-
-#asks to kill
-#ask_to_kill() {
-#	echo -e "Shall we kill a process? y/n?: "
-#	read -r  option 
-#	while :
-#	do
-#		echo -e "The Process lives another day"
-#		break  #needed to  break the loop
-#		else
-#			kill_process #go to kill process if y is the option
-#		fi
-#	done
-#}
-
-#proceed to execute
-#kill_process() {
-#	if [ "$option" == y ]; then #if option is y then run commands below
-#		echo -e "$target" 
-#		echo -e "Select your target.For your convience PID printed in output.txt"
-#		echo -e "$target">>output.txt 
-#		read -r PID 
-#		kill -9  "$PID" #kills -9 the process
-
-#		ask_to_kill #infinte loops
-#	fi
-#}
-
- 
-#ask_to_kill
-
-#end
 
 # sources
 # link to kill process https://www.linux.com/training-tutorials/how-kill-process-command-line/
