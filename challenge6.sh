5bf22a807aaf1a2119a372c68aa064b7ac02ebba
rcaoagdan <rcaoagdan@icloud.com>
2021-02-22 20:36:17 -0800

    new

---
+++ challenge6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Script Name: OPS 201 Challenge 6
+#Author: Ray Caoagdan
+#Date of last revision: 02/22/2021
+#Description of Purpose: Show Listening Ports
+
+#variables
+openports=$(sudo netstat -ltup)
+
+#print listening ports
+listening_ports () {
+    echo -e "openports"
+}
+listening_ports
