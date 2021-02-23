90ebd8578193b6708ace81f8a48dc15006027737
rcaoagdan <rcaoagdan@icloud.com>
2021-02-22 19:51:04 -0800

    change

--- challenge5.sh
+++ challenge5.sh
@@ -15,7 +15,7 @@ cat /dev/null >output.txt #deletes any content currently in output.txt
 echo -e "$process" >>output.txt #prints PID in output.txt 
 
 kill_reset () {
-	echo -e "\nShall we kill-k or reset-r a process? k/r?" #ask to kill or reset
+	echo -e "\nShall we kill-k or gracefully kill-g a process? k/g?" #ask to kill or reset
 	read -r option #reads user input
 	while :
 	do
@@ -23,7 +23,7 @@ kill_reset () {
 			kill_process #calls function kill_process
 			break #needed to break loop just in case
 		else
-			reset_process #calls function reset_process
+			grace_process #calls function grace_process
 		fi
 	done
 
@@ -38,18 +38,18 @@ kill_process() {
 	if [ "$option" == k ]; then
 		echo -e "Select process found in output.txt"
 		read -r PID
-		kill -9 "$PID"
+		kill -9 "$PID" #kill the process
 
 		kill_reset #go back to kill_rest
 	fi
 }
 
 #reset the process
-reset_process () {
+grace_process () {
 	if [ "$option" == r ]; then
 		echo -e "Select Process found in output.txt"
 		read -r PID
-		kill -15 "$PID"
+		kill -15 "$PID" #sends SIGTERM to PID (gracefully kill)
 	
 		kill_reset #go back to kill_reset
 	fi
