1e51291caa87a37f77dbf13e255e3e6f90dee074
rcaoagdan <rcaoagdan@icloud.com>
2021-02-24 02:24:47 +0000

    update

--- challenge6.sh
+++ challenge6.sh
@@ -6,12 +6,18 @@
 #Description of Purpose: Show Listening Ports
 
 #variables
-openports=$(nc -z -v 192.168.1.104 1-1023)
+#openports=$(nc -z -v 192.168.1.104 1-1023) #prints all well known ports
+
 
 #print listening ports
 listening_ports () {
     echo -e "$openports"
 }
 
+getIPaddress() {
+    echo -e "please enter an IP addresss: "
+    read -r ipaddress
+    openports=$(nc -z -v $ipaddress 1-1023)
+}
 listening_ports
 
