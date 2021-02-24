e330d5e5a5cbe9552e02a96fa6d706ed7e71c1bd
rcaoagdan <rcaoagdan@icloud.com>
2021-02-24 05:24:57 +0000

    workinprogress

--- challenge6.sh
+++ challenge6.sh
@@ -5,22 +5,49 @@
 #Date of last revision: 02/22/2021
 #Description of Purpose: Show Listening Ports
 
-
-getIPaddress() {
-    echo -e "please enter your IP addresss: "
-    read -r ipaddress
-    openports=$(nc -z -v $ipaddress 1-1023)
-}
+#sets variable set to users inputed IP ADDRESS 
+echo -e "please enter your IP addresss: "
+read -r ipaddress
+#openports=$(nc -z -v $ipaddress 1-1023)
 
 
+#main
 #print listening ports
-listening_ports () {
-    echo -e "$openports"
+print_ports () {
+    #echo -e "$ipaddress"
+    echo -e "Woud you like to view OPEN, CLOSED or ALL ports? TYPE- EXIT to leave"
+    read -r option
+    while : 
+    do 
+        if [ "$option" == OPEN ] ; then
+            open_ports
+        elif [ "$option" == CLOSED ] ; then
+            echo "closed"
+            print_ports
+        elif [ "$option" == ALL ] ; then
+            echo "ALL" 
+            print_ports
+        elif [ "$option" == EXIT ] ; then
+            echo " Have a nice day "
+            break
+        else
+            echo "Please select a correct option" #ensures on selcted parameters can be used 
+            print_ports
+        fi
+    done
+}
+
+#end
+
+#functons to be called upon to main 
+open_ports () {
+    if [ "$option" == OPEN ] ; then
+        openports=$(nc -zv $ipaddress 1-1023)
+        echo "$openports"
+        print_ports
+    fi
 }
 
 
-
-getIPaddress
-
-listening_ports
+print_ports
 
