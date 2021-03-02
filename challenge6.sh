93d05b4395a6fe29d9c1066e77dce9284ef1e6d9
rcaoagdan <rcaoagdan@icloud.com>
2021-03-01 17:52:11 -0800

    update

--- challenge6.sh
+++ challenge6.sh
@@ -5,13 +5,61 @@
 #Date of last revision: 02/22/2021
 #Description of Purpose: Show Listening Ports
 
-#variables
-openports=$(netstat --listen)
+#sets variable set to users inputed IP ADDRESS 
+echo -e "please enter your IP addresss: "
+read -r ipaddress
+#openports=$(nc -z -v $ipaddress 1-1023)
 
+
+#main
 #print listening ports
-listening_ports () {
-    echo -e "$openports"
+print_ports () {
+    #echo -e "$ipaddress"
+    echo -e "Woud you like to view OPEN, CLOSED or ALL ports? TYPE- EXIT to leave"
+    read -r option
+    while : 
+    do 
+        if [ "$option" == OPEN ] ; then
+            open_ports
+        elif [ "$option" == CLOSED ] ; then
+            close_ports
+        elif [ "$option" == ALL ] ; then
+            all_ports
+        elif [ "$option" == EXIT ] ; then
+            echo " Have a nice day "
+            break
+        else
+            echo "Please select a correct option" #ensures on selcted parameters can be used 
+            print_ports
+        fi
+    done
 }
 
-listening_ports
+#end
 
+#functons to be called upon to main 
+open_ports () {
+    if [ "$option" == OPEN ] ; then
+        openports=$(netstat -tulpn  | grep succeed)
+        echo "$openports"
+        print_ports
+    fi
+}
+
+close_ports () {
+    if [ "$option" == CLOSED ] ; then
+        closeports=$(netstat -tulpn | grep failed)
+        echo "$closedports"
+        print_ports
+    fi 
+}
+
+all_ports () {
+    if [ "$option" == ALL ] ; then
+        allports=$(netstat -tulpn | grep LISTEN) #list all ports that are listening
+        echo "$allports"
+        print_ports
+    fi
+}
+
+print_ports
