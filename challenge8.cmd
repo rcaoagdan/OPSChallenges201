a4845543a4d04bafdab22a23b853a8f532601257
rcaoagdan <rcaoagdan@icloud.com>
2021-03-03 21:13:38 -0800

    update

---
+++ challenge8.cmd
@@ -0,0 +1,9 @@
+:: Batch file saved as CMD
+
+::Ops 201 challenge 8
+::Author Raymond Caoagdan
+::Date of last revision 03/03/2021
+::Description: CMD ran on ops20104.ova to backup Jorges files.
+
+robocopy "C:\Users\j.thompson\Desktop\My Project Files" "J:\Backups\My Project Files" /E 
+::/E copies subdirectores even empty
