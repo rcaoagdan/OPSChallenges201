951fdf2c6096cb9ac93e82a501fe67fbd13ce4cc
rcaoagdan <rcaoagdan@icloud.com>
2021-03-07 17:37:55 -0800

    update

--- challenge9/challenge9.ps1
+++ challenge9/challenge9.ps1
@@ -3,5 +3,8 @@
 # Date of Last Revision : 03 / 07 / 2021
 # Purpose : Power shell script that outputs Events into determined files. Print screen evenrts with certain IDs
 
-Show-EventLog  [[-rcao]] last24
 
+
+Get-EventLog System -after (Get-Date).AddDays(-1) | Out-File -FilePath .\last_24.txt #Prints Sytem events of last 24hours to last_24.txt 
+
+Get-EventLog System -EntryType Error  | Out-File -FilePath .\error.txt #Prints out all Errors on system event logs to error.txt
