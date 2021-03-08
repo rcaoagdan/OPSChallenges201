d97f8cda252fcc121fabb6b3dc20370f852df912
rcaoagdan <rcaoagdan@icloud.com>
2021-03-07 17:07:26 -0800

    update

---
+++ challenge9/challenge9.ps1
@@ -0,0 +1,7 @@
+# Script Name : Challenge 9
+# Author : Raymond Caoagdan
+# Date of Last Revision : 03 / 07 / 2021
+# Purpose : Power shell script that outputs Events into determined files. Print screen evenrts with certain IDs
+
+Show-EventLog  [[-rcao]] last24
+
