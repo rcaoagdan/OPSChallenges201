feaa5fa0d9a0682a5dc3e65c4f2d85c2d08bff01
rcaoagdan <rcaoagdan@icloud.com>
2021-03-08 20:59:42 -0800

    work in progress

--- challenge9/challenge9.ps1
+++ challenge9/challenge9.ps1
@@ -3,20 +3,61 @@
 # Date of Last Revision : 03 / 08 / 2021
 # Purpose : Power shell script that outputs Events into determined files. Print screen evenrts with certain IDs
 
+#Variable to set to select log
+Echo "Please look over the Sytem log below:"
+Echo "1. Last 24:"
+Echo "2. Errors"
+Echo "3. Isntance ID 16"
+Echo "4. Recent 20"
+Echo "5. Recent 500"
+Echo "Please ensure a correct selection is made" 
+$logentry = Read-Host "Select the System log you wish to see"
+
+#fuctions
+function last24 {
+    
+    Get-EventLog System -after (Get-Date).AddDays(-1) | Out-File -FilePath .\last_24.txt #Prints Sytem events of last 24 hours to last_24.txt
+    $last24 = Get-EventLog System -after (Get-Date).AddDays(-1)| Select-Object -ExpandProperty message #Select-Object -ExpandProperty message used to expand entry so log is not short
+    echo $last24
+    
+}
+function errorslog {
+    
+    Get-EventLog System -EntryType Error  | Out-File -FilePath .\error.txt #Prints out all Errors on system event logs to errors
+    $logoferrors = Get-EventLog System -EntryType Error | Select-Object -ExpandProperty message
+    echo $logoferrors
+    
+}
+function instance16 {
+    $eventid16 = Get-EventLog System -InstanceId 16| Select-Object -ExpandProperty message #-InstanceId is used to select a certain event ID you wish in this case 16
+    echo $eventid16
+    
+}
+function recententry {
+   $recententries =Get-EventLog System -Newest 50| Select-Object -ExpandProperty message
+   echo $recententries
+}
+function recententry2 {
+    $recententries2 =Get-EventLog System -Newest 5 | Select-Object -ExpandProperty message
+    echo $recententries2
+ }
 #main
 
-Get-EventLog System -after (Get-Date).AddDays(-1) | Out-File -FilePath .\last_24.txt #Prints Sytem events of last 24 hours to last_24.txt
-# -after (Get-Date) will get info from days set on .AddDays(-string) where string is how many days out. 
-#outfile used to print out info to selected path with -FilePath .\string where string is where you want the info printed too. | >> last_24.txt also works
+    if($logentry -eq 1){
+        last24
+    }elseif($logentry -eq 2){
+        errorslog
+    }elseif($logentry -eq 3){
+        instance16
+    }elseif($logentry -eq 4){
+        recententry
+    }elseif($logentry -eq 5){
+        recententry2
+    }else{
+        echo "No Correct Selection Made, exiting "
+    }
 
-Get-EventLog System -EntryType Error  | Out-File -FilePath .\error.txt #Prints out all Errors on system event logs to errors
-# -EntryType is where you select what entry type you wish in the system log
-
-Get-EventLog System -InstanceId 16 #-InstanceId is used to select a certain event ID you wish in this case 16
-
-Get-EventLog System -Newest 20 # Newest used followed by parameter to print out newest entry. 
-
-$Events = Get-EventLog System -Newest 500 #varaible set to print 500 system log entries. 
-$Events | Select-Object -ExpandProperty message #select -expandproperty used to extend the message so the printed info is not shortened 
+  
 
 #end
+
