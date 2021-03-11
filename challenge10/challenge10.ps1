cd4cea25e406fa4de75d4bfcea8ef8fb605f5880
rcaoagdan <rcaoagdan@icloud.com>
2021-03-10 18:17:03 -0800

    working

--- challenge10/challenge10.ps1
+++ challenge10/challenge10.ps1
@@ -2,3 +2,9 @@
 #Autor: Raymond Caoagdan
 #Date of last revision : 03/10/2021
 #Purpose : System Process Powershell commands 
+
+#Variables
+$CPUTIME = Get-Process | Format-Table -View priority
+
+
+Echo $CPUTIME
