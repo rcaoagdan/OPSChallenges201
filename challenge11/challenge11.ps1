529ed8dc9e2cd2ab34a103fc9d2e2dbe15a75716
rcaoagdan <rcaoagdan@icloud.com>
2021-03-13 22:28:24 -0800

    new challenge 11

---
+++ challenge11/challenge11.ps1
@@ -0,0 +1,5 @@
+#Script Name : Challenge 11
+#Autor: Raymond Caoagdan
+#Date of last revision : 03/13/2021
+#Purpose : Automated Endpoint configurations
+# Automate system configurations on Windows 10
