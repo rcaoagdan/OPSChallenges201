5e083e3100a462a0ca5cd617782133dccb038545
rcaoagdan <rcaoagdan@icloud.com>
2021-03-15 08:58:32 -0700

    newly update

--- challenge11/challenge11.ps1
+++ challenge11/challenge11.ps1
@@ -4,4 +4,15 @@
 #Purpose : Automated Endpoint configurations
 # Automate system configurations on Windows 10
 
-Show-NetFirewallRule 
+# Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled True #Turn on File and Printer Sharing
+# Get-NetFirewallRule -Name (File and Printer Sharing) #view status of file and printer sharing
+
+ # netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4 #Allow ICMP traffic
+
+ # reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f #remtoe management
+
+ # iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat')) #remote bloateware
+
+ # Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All #enable hyper v
+
+ # Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force #smb1 disabled
