92da2c470375653dcac8274fb7b45a6fafac9426
rcaoagdan <rcaoagdan@icloud.com>
2021-03-15 20:41:08 -0700

    update

--- challenge11/challenge11.ps1
+++ challenge11/challenge11.ps1
@@ -4,8 +4,43 @@
 #Purpose : Automated Endpoint configurations
 # Automate system configurations on Windows 10
 
+#variables
+$start = true
+$printrule = Get-NetFirewallRule -DisplayGroup 'File and Printer Sharing'
+$viewremoterules = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server'
+$viewbloat = DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename
+
+#fuctions
+function mainfunction {
+    Write-Output "Hello, welcome to self automation, what would you like to do?"
+    Write-Output "1. Enable File and Printer Sharing"
+    Write-Output "2. Allow ICMP traffic"
+    Write-Output "3. Enable Remote management"
+    Write-Output "4. Remove bloatware"
+    Write-Output "5. Enable hyper-v"
+    Write-Output "6. Disable SMBv1"
+    Write-Output "7.Exit"
+
+}
+function fileandprint {
+    Write-Output "Here are the current File and Printer Sharing settings"
+    Write-Output $printrule
+    $fileprintinput = Read-Host Shall we enable y/n?
+    if ($fileprintinput -eq "y") {
+        Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled True
+    }
+    elseif ($fileprintinput -eq "n") {
+        Write-Output "File and Printer Sharing shall be left alone"
+        mainfunction
+    }
+    else {
+        Write-Output "PLease enter correct entry"
+        fileandprint
+    }
+}
+
 # Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled True #Turn on File and Printer Sharing
-# Get-NetFirewallRule -Name (File and Printer Sharing) #view status of file and printer sharing
+ #Get-NetFirewallRule -DisplayGroup 'File and Printer Sharing' #view status of file and printer sharing
 
  # netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4 #Allow ICMP traffic
 
@@ -15,9 +50,13 @@
 
  # iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat')) #remove bloateware
  # Get-AppxPackage -name " " | Remove-AppxPackage #removebloatware
-# DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename #view bloatware
+# DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename #view bloatware run as adminstrator
 # DISM /Online /Remove-ProvisionedAppxPackage /PackageName:PACKAGENAME #remove bloatware
 
  # Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All #enable hyper v
+ # DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V #DISM hyper v 
+# Get-Command -Module hyper-v | Out-GridView # list hyperv commands
 
  # Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force #smb1 disabled
+#mainfunction
+fileandprint
