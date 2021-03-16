78bafffdf023bc83318d89fc1fe84c5fd2c7aef6
rcaoagdan <rcaoagdan@icloud.com>
2021-03-15 18:33:33 -0700

    working

--- challenge11/challenge11.ps1
+++ challenge11/challenge11.ps1
@@ -10,8 +10,13 @@
  # netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4 #Allow ICMP traffic
 
  # reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f #remtoe management
+ # Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' #view remote management
 
- # iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat')) #remote bloateware
+
+ # iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat')) #remove bloateware
+ # Get-AppxPackage -name " " | Remove-AppxPackage #removebloatware
+# DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename #view bloatware
+# DISM /Online /Remove-ProvisionedAppxPackage /PackageName:PACKAGENAME #remove bloatware
 
  # Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All #enable hyper v
 
