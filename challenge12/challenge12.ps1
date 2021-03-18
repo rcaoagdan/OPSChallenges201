252c4c750b45e0e39bdcf719bc2fbb63dfe1234b
rcaoagdan <rcaoagdan@icloud.com>
2021-03-17 20:07:25 -0700

    update

--- challenge12/challenge12.ps1
+++ challenge12/challenge12.ps1
@@ -1,15 +1,8 @@
 #Script Name : Challenge 12
 # Author : Raymond Caoagdan
-# Date 03/16/2021
+# Date 03/17/2021
 # Purpose : Powershell IP Analysis | String
 
-#variables
-
-
-#Script Name : Challenge 12
-# Author : Raymond Caoagdan
-# Date 03/16/2021
-# Purpose : Powershell IP Analysis | String
 
 #variables
 $break = Write-Output " " #line break var
