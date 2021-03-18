13f8723d2bdb37c8cc1f2d9c4e6eeb58bae6dcb7
rcaoagdan <rcaoagdan@icloud.com>
2021-03-17 22:16:58 -0700

    more clean up

--- challenge12/challenge12.ps1
+++ challenge12/challenge12.ps1
@@ -1,8 +1,7 @@
 #Script Name : Challenge 12
 # Author : Raymond Caoagdan
-# Date 03/17/2021
+# Date 03/16/2021
 # Purpose : Powershell IP Analysis | String
-
 
 #variables
 $break = Write-Output " " #line break var
@@ -47,52 +46,46 @@ function ipconfigurations {
     Write-Output "2. IPv6"
     Write-Output "3. Subnet"
     Write-Output "4. Default Gateway"
-    Write-Output "5. DNS Suffix"
-    Write-Output "6.Return to main menu"
+    Write-Output "5. Return to main menu"
     $ipinput = Read-Host "What would you like to do?"
     
     if ($ipinput -eq 1) {
         $break
-        ipconfig /all | findstr /v 00-00-00 | Select-String IPv4
+        ipconfig /all | findstr /v 00-00-00 | Select-String IPv4 | select-object -First 1
         $break
         ipconfigurations
     }
     elseif ($ipinput -eq 2) {
         $break
-        ipconfig /all | findstr /v 00-00-00 | Select-String IPv6
+        ipconfig /all | findstr /v 00-00-00 | Select-String IPv6 | select-object -First 1
         $break
         ipconfigurations
        
     }
     elseif ($ipinput -eq 3) {
         $break
-        ipconfig /all | findstr /v 00-00-00 | Select-String "Subnet Mask"
+        ipconfig /all | findstr /v 00-00-00 | Select-String "Subnet Mask" | select-object -First 1
         $break
         ipconfigurations
     }
     elseif ($ipinput -eq 4) {
         $break
-        ipconfig /all | findstr /v 00-00-00 | Select-String "Default Gateway"
+        ipconfig /all | findstr /v 00-00-00 | Select-String "Default Gateway" | select-object -First 1
         $break
         ipconfigurations
     }
     elseif ($ipinput -eq 5) {
         $break
-        ipconfig /all | findstr /v 00-00-00 | Select-String "Connection-specific DNS Suffix"
-        $break
-        ipconfigurations
-    }
-    elseif ($ipinput -eq 6) {
-        $break
         mainone
     }
+   
     else {
         $break
         $incorrect
         $break
         ipconfigurations
     }
-} #ipconfig uses pipping ' | 'to store information to be called upon
+} #this function has all ipconfig use pipping to store information to be called upon
 
 function testinternet {
     Write-Output "Shall we test internet connection?"
@@ -102,7 +95,7 @@ function testinternet {
     $testinter = Read-Host "What would you like to do?"
     if ($testinter -eq 1){
         Test-NetConnection 
-        testinternet
+	testinternet
     }
     elseif ($testinter -eq 2) {
         pingfunc
@@ -129,7 +122,7 @@ function pingfunc {
    }
    elseif ($pingYN -eq 'n') {
        $break
-       testinternet
+       mainone
    }
    else {
        $break
@@ -137,7 +130,7 @@ function pingfunc {
        $break
    }
     
-} 
+}
 #main
 mainone
 #end
@@ -145,4 +138,6 @@ mainone
 # sources
 # ipconfig https://www.meridianoutpost.com/resources/articles/command-line/ipconfig.php
 # net packets https://devblogs.microsoft.com/scripting/gathering-network-statistics-with-powershell/
+
+
 
