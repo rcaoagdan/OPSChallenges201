bf834f9a1171bbf6b00cfc9fd2a15b3fab6c962a
rcaoagdan <rcaoagdan@icloud.com>
2021-03-21 13:38:45 -0700

    new

---
+++ challenge13/challenge13.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#Script: Ops Challenge 13
+#Author: Ray Caoagdan
+#Date of Last Revision: 03/21/21
+#Purpose: Arrays : Bash Domain analysis 
+
+#variables
+echo -e "\nPlease enter a domain name?"
+read -r ipvar 
+
+echo $ipvar 
