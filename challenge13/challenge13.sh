5189b4bed7d47d5b12ac9abb45ce13d4a0421046
rcaoagdan <rcaoagdan@icloud.com>
2021-03-22 21:12:10 -0700

    update

--- challenge13/challenge13.sh
+++ challenge13/challenge13.sh
@@ -9,10 +9,13 @@
 echo -e "\nPlease enter a domain name?"
 read -r ipvar 
 
+
 #Main Function utilizeing whois, dig, nslookup
 
 main_function (){
-    whoisfunction 
+    #echo -e "\nPlease enter a domain name?"
+    #read -r ipvar 
+    whoisfunction
     dig $ipvar
     host $ipvar
     nslookup $ipvar
@@ -31,11 +34,17 @@ whoisfunction (){
     whois $ipvar| grep -m1  'Registrar IANNA ID'
     whois $ipvar| grep -m1  'Registrar Abuse Contact Email'
     whois $ipvar| grep -m1  'Registrar Abuse Contact Phone'
-    whois $ipvar| grep -m4  'Domain Status'
-    whois $ipvar| grep -m4  'Name Server'
+    whois $ipvar| grep -m1  'Domain Status'
+    whois $ipvar| grep -m1 'Name Server'
     whois $ipvar| grep -m1  'DNSSEC'
 }
 #main
-regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~|)'
-main_function >> challenge13.txt
+if [[ $(curl $ipvar) ]] 2>/dev/null; #sees url valid
+ then
+  main_function >> challenge13.txt
+ else
+  echo "This page does not exist."
+  main_function
+fi
+    
 #end 
