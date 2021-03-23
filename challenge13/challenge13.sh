821f58af8cba705d8154514a4622d63dff21c76e
rcaoagdan <rcaoagdan@icloud.com>
2021-03-22 20:47:27 -0700

    working

--- challenge13/challenge13.sh
+++ challenge13/challenge13.sh
@@ -12,12 +12,30 @@ read -r ipvar
 #Main Function utilizeing whois, dig, nslookup
 
 main_function (){
-    whois $ipvar
+    whoisfunction 
     dig $ipvar
     host $ipvar
     nslookup $ipvar
 }
 
+# whois function using grep to only display usefull infor
+whoisfunction (){
+    whois $ipvar| grep -m1  'Domain Name'
+    whois $ipvar| grep -m1  'Registry Domain ID'
+    whois $ipvar| grep -m1  'Registrar WHOIS Server'
+    whois $ipvar| grep -m1  'Registrar URL'
+    whois $ipvar| grep -m1  'Updated Date'
+    whois $ipvar| grep -m1  'Creation Date'
+    whois $ipvar| grep -m1  'Registry Exipry Date'
+    whois $ipvar| grep -m1  'Registrar'
+    whois $ipvar| grep -m1  'Registrar IANNA ID'
+    whois $ipvar| grep -m1  'Registrar Abuse Contact Email'
+    whois $ipvar| grep -m1  'Registrar Abuse Contact Phone'
+    whois $ipvar| grep -m4  'Domain Status'
+    whois $ipvar| grep -m4  'Name Server'
+    whois $ipvar| grep -m1  'DNSSEC'
+}
 #main
+regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~|)'
 main_function >> challenge13.txt
 #end 
