de2afe16dbcc7a842246b51952c14d1824b7636d
rcaoagdan <rcaoagdan@icloud.com>
2021-03-23 19:08:25 -0700

    final

--- challenge13/challenge13.sh
+++ challenge13/challenge13.sh
@@ -2,7 +2,7 @@
 
 #Script: Ops Challenge 13
 #Author: Ray Caoagdan
-#Date of Last Revision: 03/21/21
+#Date of Last Revision: 03/23/21
 #Purpose: Arrays : Bash Domain analysis 
 
 #variables
@@ -13,32 +13,29 @@ read -r ipvar
 #Main Function utilizeing whois, dig, nslookup
 
 main_function (){
-    #echo -e "\nPlease enter a domain name?"
-    #read -r ipvar 
-    #whoisfunction
-    whois $ipvar
+    whoisfunction
     dig $ipvar
     host $ipvar
     nslookup $ipvar
 }
 
 # whois function using grep to only display usefull infor
-#whoisfunction (){
-   # whois $ipvar| grep -m1  'Domain Name'
-   # whois $ipvar| grep -m1  'Registry Domain ID'
-    #whois $ipvar| grep -m1  'Registrar WHOIS Server'
-   # whois $ipvar| grep -m1  'Registrar URL'
-   # whois $ipvar| grep -m1  'Updated Date'
-    #whois $ipvar| grep -m1  'Creation Date'
-    #whois $ipvar| grep -m1  'Registry Exipry Date'
-    #whois $ipvar| grep -m1  'Registrar'
-    #whois $ipvar| grep -m1  'Registrar IANNA ID'
-    #whois $ipvar| grep -m1  'Registrar Abuse Contact Email'
-    #whois $ipvar| grep -m1  'Registrar Abuse Contact Phone'
-    #whois $ipvar| grep -m1  'Domain Status'
-    #whois $ipvar| grep -m1 'Name Server'
-    #whois $ipvar| grep -m1  'DNSSEC'
-#}
+whoisfunction (){
+    whois $ipvar| grep -m1  'Domain Name'
+    whois $ipvar| grep -m1  'Registry Domain ID'
+    whois $ipvar| grep -m1  'Registrar WHOIS Server'
+    whois $ipvar| grep -m1  'Registrar URL'
+    whois $ipvar| grep -m1  'Updated Date'
+    whois $ipvar| grep -m1  'Creation Date'
+    whois $ipvar| grep -m1  'Registry Exipry Date'
+    whois $ipvar| grep -m1  'Registrar'
+    whois $ipvar| grep -m1  'Registrar IANNA ID'
+    whois $ipvar| grep -m1  'Registrar Abuse Contact Email'
+    whois $ipvar| grep -m1  'Registrar Abuse Contact Phone'
+    whois $ipvar| grep -m1  'Domain Status'
+    whois $ipvar| grep -m1 'Name Server'
+    whois $ipvar| grep -m1  'DNSSEC'
+}
 #main
 if [[ $(curl $ipvar) ]] 2>/dev/null; #sees url valid
  then
